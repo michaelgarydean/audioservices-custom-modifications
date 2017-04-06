fa0dfbac852574f7f9c4c0fc781339eb2cd459dc
Mike Dean <contact@michaeldean.ca>
2017-04-06 21:22:01 +0000

    removed order actions and customer message

--- audioservices-custom-modifications.php
+++ audioservices-custom-modifications.php
@@ -19,9 +19,9 @@
  * @param String $currency		The currency's acronym.
  * @return String $currency_symbol	The replacement text to use for the currency symbol.
  */
-add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10, 2);
+add_filter( 'woocommerce_currency_symbol', 'pheek_change_existing_currency_symbol', 10, 2 );
 
-function change_existing_currency_symbol( $currency_symbol, $currency ) {
+function pheek_change_existing_currency_symbol( $currency_symbol, $currency ) {
 	switch( $currency ) {
 		case 'USD': 
 			$currency_symbol = '$USD '; 
@@ -34,3 +34,24 @@ function change_existing_currency_symbol( $currency_symbol, $currency ) {
 	return $currency_symbol;
 }
 
+/**
+ * Remove admin columns in the Woocommerce orders admin menu.
+ * wp-admin/edit.php?post_type=shop_order
+ */
+
+add_filter( 'manage_edit-shop_order_columns', 'pheek_remove_woocommerce_admin_columns', 15 );
+
+function pheek_remove_woocommerce_admin_columns( $columns ) {
+	$new_columns = ( is_array( $columns ) ) ? $columns : array();
+	
+	//Remove column
+	unset( $new_columns[ 'customer_message' ] );
+	unset( $new_columns[ 'order_actions' ] );
+	return $new_columns;
+}
+
+add_action( 'manage_shop_order_posts_custom_column', 'pheek_custom_column', 10, 2 );
+
+function pheek_custom_column( $column ) {
+	global $post, $woocommerce, $the_order;	
+}
